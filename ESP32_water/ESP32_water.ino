4d3ba96ee65a6080d0b36c3e49f5266ba7c6bc79
uthainantang <44509444+uthainantang@users.noreply.github.com>
2023-09-18 22:09:57 +0700

    Update ESP32_water.ino

--- ESP32_water/ESP32_water.ino
+++ ESP32_water/ESP32_water.ino
@@ -1,6 +1,19 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <PZEM004Tv30.h>
+#include "DHT.h"
+
+#define DHTPIN 13 
+#define water_level_pin 14
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#if defined(ESP32)
+PZEM004Tv30 pzem(Serial2, 16, 17);
+#else
+PZEM004Tv30 pzem(Serial2);
+#endif 
 
 const char* ssid = "home_nantang";
 const char* password = "0622780667";
@@ -8,8 +21,13 @@ const char* serverName = "http://49.0.124.54:1234/water_monitors";
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
 
+float current_pzem-004T;
+float temp;
+int water_level =0;
 void setup() {
   Serial.begin(115200);
+  dht.begin();
+  
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -22,6 +40,19 @@ void setup() {
 }
 
 void loop() {
+
+  current_pzem-004T = pzem.current();
+  if (isnan(current)) {
+    Serial.println("Error reading current");
+  }
+  temp = dht.readTemperature();
+  water_level = analogRead(water_level_pin);
+  Serial.print("current_pzem-004T =");
+  Serial.println(current_pzem-004T);
+  Serial.print("temp =");
+  Serial.println(temp);
+  Serial.print("water_level =");
+  Serial.println(water_level);
   //Send an HTTP POST request every 10 minutes
   if ((millis() - lastTime) > timerDelay) {
     //Check WiFi connection status
@@ -34,8 +65,8 @@ void loop() {
 
       JSONVar waterMonitor;
       waterMonitor["mac_address"] = "00:11:22:33:44:55";
-      waterMonitor["electric_current"] = 3.14159265;
-      waterMonitor["water_temperature"] = 24.25;
+      waterMonitor["electric_current"] = current_pzem-004T;
+      waterMonitor["water_temperature"] = temp;
       waterMonitor["water_flow"] = true;
 
       String jsonString = JSON.stringify(waterMonitor);
