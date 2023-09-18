aefd4aaa903c3bb3a886fcc7e153526b66555412
uthainantang <44509444+uthainantang@users.noreply.github.com>
2023-09-18 17:45:25 +0700

    Create ESP32_water.ino

---
+++ ESP32_water/ESP32_water.ino
@@ -0,0 +1,74 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
+const char* ssid = "home_nantang";
+const char* password = "0622780667";
+
+const char* serverName = "http://49.0.124.54:1234/water_monitors";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
+      WiFiClient client;
+      HTTPClient http;
+
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+      // Specify content-type header
+      //http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // Data to send with HTTP POST
+      //String httpRequestData = "mac_address=00:11:22:33:44:55&electric_current=3.14159265&water_temperature=25.12&water_flow=true";
+      // Send HTTP POST request
+      //int httpResponseCode = http.POST(httpRequestData);
+
+      // If you need an HTTP request with a content type: application/json, use the following:
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST("{\"mac_address\":\"00:11:22:33:44:55\",\"electric_current\":\"3.14159265\",\"water_temperature\":\"24.25\",\"water_flow\":\"true\"}");
+      
+      // Add values in the document
+      //int t = 27.5;
+      //doc["mac_address"] = "00:11:22:33:44:55";
+      //doc["electric_current"] = 3.14159265;
+      //doc["water_temperature"] = 25.5;
+      //doc["water_flow"] = " true";
+      //String requestBody;
+      //serializeJson(doc, requestBody);
+      
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
